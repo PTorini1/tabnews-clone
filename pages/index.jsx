cee91cf0d672e1d48800a4d0b2a4f7d444c845fd
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2023-09-14 17:41:16 +0000

    feat: creating impacting message

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>QQ COISA AÍ</h1>;
+    return <div>
+        <h2>Gato sem vergonha passando na sua tela</h2>
+        <img src="https://conexaoplaneta.com.br/wp-content/uploads/2023/07/comedy-pet-abre-conexao-planeta-1024x708.jpg"></img>
+    </div>;
 }
 
 export default Home;
