08082d3fe0e5e972414aec721586de92c3d13d9d
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2023-11-01 20:54:26 +0000

    feat: adding prettier configurations

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # tabnews-clone
+
 https://tabnews-clone-ptorini1.vercel.app
 
 Project to study JS frameworks and understand concepts of programming
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,18 @@
 function Home() {
-    return <div>
-        <h2>Gato sem vergonha passando na sua tela</h2>
-        <img src="https://conexaoplaneta.com.br/wp-content/uploads/2023/07/comedy-pet-abre-conexao-planeta-1024x708.jpg"></img>
-    </div>;
+  return (
+    <div>
+      <h2>Gato sem vergonha passando na sua tela</h2>
+      <img src="https://conexaoplaneta.com.br/wp-content/uploads/2023/07/comedy-pet-abre-conexao-planeta-1024x708.jpg"></img>
+    </div>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste2");
 }
 
 export default Home;
