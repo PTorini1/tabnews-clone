ce92b2c8eb311702e4131e76232a7b4b713359c8
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2024-03-03 17:41:02 -0300

    feat: updating tests

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,11 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 AS SUM");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ valor: "alunos do curso.dev são pessoas legais" });
+  const updateAt = new Date().toISOString();
+
+  response.status(200).json({
+    updated_at: updateAt,
+  });
 }
 
 export default status;
