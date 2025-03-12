1d7582463314b77c29c772385d897400a4817737
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2025-03-12 00:18:39 +0000

    feat: Initial configuration for jest transpilation

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,7 +1,10 @@
+import databse from "infra/database.js";
+
 test("GET to api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
