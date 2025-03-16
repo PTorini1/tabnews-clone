5e71baadb6af0ad063588232302bf6300056551c
PTorini1 <102087586+PTorini1@users.noreply.github.com>
2025-03-16 12:28:00 +0000

    feat: Improving migrations tests

--- infra/database.js
+++ infra/database.js
@@ -1,27 +1,17 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
-  let result = "";
+  let client;
   try {
-    await client.connect();
-    result = await client.query(queryObject);
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
   } catch (error) {
     console.log(error);
     throw error;
   } finally {
     await client.end();
   }
-
-  return result;
 }
 
 async function getDatabaseVersion() {
@@ -55,9 +45,24 @@ function getSSLValues() {
   return process.env.NODE_ENV === "production" ? true : false;
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
-  getDatabaseVersion: getDatabaseVersion,
-  getActiveConnections: getActiveConnections,
-  getMaxConnections: getMaxConnections,
+  query,
+  getNewClient,
+  getDatabaseVersion,
+  getActiveConnections,
+  getMaxConnections,
 };
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,12 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
   const defaultMigrationOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -13,6 +16,7 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
     return response.status(200).json(pendingMigrations);
   }
 
@@ -21,6 +25,8 @@ export default async function migrations(request, response) {
       ...defaultMigrationOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
 
     if (migratedMigrations.length > 0)
       return response.status(201).json(migratedMigrations);
